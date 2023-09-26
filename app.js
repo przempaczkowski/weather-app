32ced7c08906d11d768bdd49c65858e9f1867f31
przempaczkowski <25874300+przempaczkowski@users.noreply.github.com>
2023-09-26 22:28:14 +0100

    active search bar

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
 const apiKey = "2718fcf9bd8838aeb76b3339e551a3b1";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
-// const searchBox = document.querySelector(".search input");
-// const searchBtn = document.querySelector(".search button");
+const searchBox = document.querySelector(".search-input");
+const searchBtn = document.querySelector(".search-button");
 
-async function checkWeather(){
-    const response = await fetch(apiUrl + "Wroclaw" + `&appid=${apiKey}`);
-    var data = await response.json();
-
-    console.log(data);
-
-    document.querySelector(".location").innerHTML = data.name;
-    document.querySelector(".current-temperature").innerHTML = Math.round(data.main.temp) + "°C";
-    document.querySelector(".wind").innerHTML = Math.round(data.wind.speed) + " km/h";
-    document.querySelector(".pressure").innerHTML = data.main.pressure + " hPa";
+async function checkWeather(location) {
+    try {
+        const response = await fetch(apiUrl + location + `&appid=${apiKey}`);
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        console.log(data);
+        document.querySelector(".location").textContent = data.name;
+        document.querySelector(".current-temperature").textContent =
+            Math.round(data.main.temp) + "°C";
+        document.querySelector(".wind").textContent =
+            Math.round(data.wind.speed) + " km/h";
+        document.querySelector(".pressure").textContent =
+            data.main.pressure + " hPa";
+    } catch (error) {
+        console.error("Error fetching weather data:", error);
+    }
 }
 
-checkWeather();
-// searchBtn.addEventListener("click", ()=>{
-//     checkWeather(searchBox.value);
-// })
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value);
+});
