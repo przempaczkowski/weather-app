58471fe740fe63941cd0bdf94f8fa8d582a2dc19
przempaczkowski <25874300+przempaczkowski@users.noreply.github.com>
2023-09-27 23:23:09 +0100

    5days forecast

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 
 const apiKey = "2718fcf9bd8838aeb76b3339e551a3b1";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const forecastApiUrl = "https://api.openweathermap.org/data/2.5/forecast?units=metric&";
 const searchBox = document.querySelector(".search-input");
 const searchBtn = document.querySelector(".search-button");
 const weatherIcon = document.querySelector(".weather-icon");
@@ -15,13 +16,44 @@ async function checkWeather(location) {
         console.log(response);
         const data = await response.json();
         console.log(data);
+        const lat = data.coord.lat;
+        console.log(lat);
+        const lon = data.coord.lon;
+        console.log(lon);
+        const cityName = data.name;
+        console.log(cityName);
+
+        const forecast = await fetch(forecastApiUrl + `lat=${lat}` + `&lon=${lon}` + `&appid=${apiKey}`);
+        const forecastdata = await forecast.json();
+        console.log(forecastdata);
+
+        // current data
         document.querySelector(".location").textContent = data.name;
         document.querySelector(".current-temperature").textContent =
             Math.round(data.main.temp) + "°C";
         document.querySelector(".wind").textContent =
             Math.round(data.wind.speed) + " km/h";
-        document.querySelector(".pressure").textContent =
-            data.main.pressure + " hPa";
+        document.querySelector(".feels-like").textContent =
+            Math.round(data.main.feels_like) + "°C";
+
+        // forecasted data
+        document.querySelector(".day1").textContent = forecastdata.list[5].dt_txt.split(" ")[0];
+        document.querySelector(".day2").textContent = forecastdata.list[13].dt_txt.split(" ")[0];
+        document.querySelector(".day3").textContent = forecastdata.list[21].dt_txt.split(" ")[0];
+        document.querySelector(".day4").textContent = forecastdata.list[29].dt_txt.split(" ")[0];
+        document.querySelector(".day5").textContent = forecastdata.list[37].dt_txt.split(" ")[0];
+
+        document.querySelector(".max1").textContent = Math.round(forecastdata.list[5].main.temp_min) + "°C";
+        document.querySelector(".max2").textContent = Math.round(forecastdata.list[13].main.temp_min) + "°C";
+        document.querySelector(".max3").textContent = Math.round(forecastdata.list[21].main.temp_min) + "°C";
+        document.querySelector(".max4").textContent = Math.round(forecastdata.list[29].main.temp_min) + "°C";
+        document.querySelector(".max5").textContent = Math.round(forecastdata.list[37].main.temp_min) + "°C";
+
+        document.querySelector(".min1").textContent = Math.round(forecastdata.list[2].main.temp_max) + "°C";
+        document.querySelector(".min2").textContent = Math.round(forecastdata.list[10].main.temp_max) + "°C";
+        document.querySelector(".min3").textContent = Math.round(forecastdata.list[18].main.temp_max) + "°C";
+        document.querySelector(".min4").textContent = Math.round(forecastdata.list[26].main.temp_max) + "°C";
+        document.querySelector(".min5").textContent = Math.round(forecastdata.list[34].main.temp_max) + "°C";
 
         if(data.weather[0].main == "Clouds") {
             weatherIcon.src = "images/cloudy.svg"
